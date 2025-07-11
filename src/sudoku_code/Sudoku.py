77d417d26063c067bcd83b487e1555bef09559d5
NoleStites <nole.stites@gmail.com>
2025-07-11 11:07:58 -0500

    Variable Flask port

--- src/sudoku_code/Sudoku.py
+++ src/sudoku_code/Sudoku.py
@@ -532,4 +532,4 @@ if __name__ == "__main__":
     backtracking = False # To keep track if we choose the Tile to collapse or not
     history = []
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
